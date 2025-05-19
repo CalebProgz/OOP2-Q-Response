e8ca97e61a500cf3cd1fb30ef72baeb8b153f6bd
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-05-19 03:44:39 +0300

    Updated login page UI and icons

--- src/main/java/com/MediLink/OOP2_Project/LogInController.java
+++ src/main/java/com/MediLink/OOP2_Project/LogInController.java
@@ -1,4 +1,13 @@
 package com.MediLink.OOP2_Project;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LogInController {
+
+    @GetMapping("/")
+    public String login() {
+        return "login"; // This tells Spring to look for templates/login.html
+    }
 }
